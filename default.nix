ac79ab044245ab3f37a98944660ffcc1fa96411d
Jichao Ouyang <oyanglulu@gmail.com>
2026-02-05 20:25:58 +1100

    upgrade to version 25.11

--- default.nix
+++ default.nix
@@ -1,6 +1,9 @@
-{pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/156692232552.tar.gz") {}}:
-let armpkgs = import pkgs.path { crossSystem = { config = "armv7l-unknown-linux-gnueabi"; }; };
+let pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/809d16bdf5ab.tar.gz") {};
+    armpkgs = import pkgs.path {
+      crossSystem = {
+        config = "armv7l-unknown-linux-gnueabi"; }; };
 in {
   server = armpkgs.vaultwarden;
-  ui = pkgs.vaultwarden-vault;
+  ui = pkgs.vaultwarden.webvault;
+  cloudflared = armpkgs.cloudflared;
 }
